c3e63adae6a63e1d886b4a928cf874fce1504f7a
Jhonny Mendoza <jhonnymendoza@Mac-mini-de-Jhonny.local>
2021-10-27 01:12:48 -0300

    creacion de funciones

--- app.js
+++ app.js
@@ -4,4 +4,33 @@ const sum = (a,b) => {
 
 //console.log(sum(7,3));
 
-module.exports={ sum }
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+const {JPY,USD,GBP} = oneEuroIs;
+
+const fromEuroToDollar = (euro) =>{
+    
+    let valorEuro = euro * USD;
+    return valorEuro
+}
+console.log(fromEuroToDollar(3.5));
+
+const fromDollarToYen =(dolar) =>{
+     let valorDolar = fromEuroToDollar((dolar)*JPY);
+     return valorDolar;
+}
+console.log(fromDollarToYen(2));
+
+const fromYenToPound = (yen) => {
+    let valorYen = Math.round(yen * (GBP/JPY));
+    return valorYen;
+}
+console.log(fromYenToPound(200));
+
+
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
