6b50be16fef098fea891c8c5d2b3da68e4358309
Jhonny Mendoza <jhonnymendoza@Mac-mini-de-Jhonny.local>
2021-10-27 01:22:55 -0300

    logica de los test unitarios de las funciones

--- test.js
+++ test.js
@@ -6,3 +6,27 @@ test('adds 14 + 9 to equal 23', ()=> {
 
     expect(total).toBe(23);
 });
+
+const { fromEuroToDollar } = require('./app.js');
+
+test( '10 dolares son 12 euros', ()=>{
+
+    let total = fromEuroToDollar(10);
+    expect(total).toBe(12)
+});
+
+const { fromDollarToYen } = require('./app.js');
+
+test ( '2 dolares son 306.96', ()=>{
+
+    let total = fromDollarToYen(2);
+    expect(total).toBe(306.96)
+});
+
+const { fromYenToPound } = require('./app.js');
+
+test ( '20 yenes son 1,25 pound ', () =>{
+
+    let total = fromYenToPound(200);
+    expect(total).toBe(1,25)
+});
